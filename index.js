99c9aab14e01104dd7acd3d948a777cb70057357
“Mimta <mimta.chowdhury@gmail.com>
2025-03-15 18:41:38 -0400

    add use routers requests

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
-// initialize Express in project
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
-import photos from "./routes/photos.js"
-import tags from "./routes/tags.js"
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import photos from "./routes/photos.js";
+import tags from "./routes/tags.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 const { CORS_ORIGIN } = process.env;
-app.use(cors({ origin: CORS_ORIGIN}));
+app.use(cors({ origin: CORS_ORIGIN }));
 
-// enable JSON parsing
+// Enable JSON parsing
 app.use(express.json());
 
-// static images
+// Serve static images
 app.use("/images", express.static("./public/images"));
 
-app.get("/tags", tags);
-app.get("/photos", photos);
-app.get("/photos/:id", photos);
-app.get("/photos/:id/comments", photos);
-app.post("/photos/:id/comments", photos);
+// Use routers
+app.use("/photos", photos);
+app.use("/tags", tags);
 
-// start server
-app.listen(PORT, () => 
+// Start server
+// Change server startup to:
+app.listen(PORT, '0.0.0.0', () => 
     console.log(`Server running on port ${PORT}`)
 );
