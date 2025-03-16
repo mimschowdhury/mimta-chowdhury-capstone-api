ddef3e22810d4730ecb7f775b7af472d37dce1d2
“Mimta <mimta.chowdhury@gmail.com>
2025-03-16 18:02:20 -0400

    remove commented out code in tags.js

--- routes/tags.js
+++ routes/tags.js
@@ -1,31 +1,3 @@
-// import express from "express";
-// import fs from "fs";
-
-// const router = express.Router();
-
-// function readTags() {
-//     try {
-//         const tagsData = fs.readFileSync("./data/tags.json");
-//         const parsedData = JSON.parse(tagsData);
-//         return parsedData;
-//     } catch (error) {
-//         console.error("Error reading or parsing tags file:", error);
-//         return null;
-//     }
-// }
-
-// router.get("/tags", (req, res) => {
-//     try {
-//         const tags = readTags();
-//         res.json(tags);
-//     } catch (error) {
-//         console.error("Unexpected error in /tags route:");
-//         res.status(500).json({ message: error.message });
-//     }
-// });
-
-// export default router;
-
 import express from "express";
 import initKnex from "knex";
 import configuration from "../knexfile.js";
